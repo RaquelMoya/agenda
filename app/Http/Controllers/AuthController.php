bc477c0e40e031597319d22d9ae0324ed3a11c0f
raquelMoya <rakelms90@gmail.com>
2022-04-08 15:29:03 +0200

    JWT implemented. UserController created

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Models\User; 
+use Illuminate\Http\Request; 
+use Symfony\Component\HttpFoundation\Response; 
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+
+
+?>
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
