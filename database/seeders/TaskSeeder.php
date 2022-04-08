8e95d595c67b007672a45d7f6190185099cc40a0
raquelMoya <rakelms90@gmail.com>
2022-04-08 15:58:41 +0200

    User & Task Seeder created. Reverse engineer created

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class TaskSeeder extends Seeder
 {
@@ -13,6 +15,22 @@ class TaskSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('tasks')->insert([
+            'title' => Str::random(10),
+            'description' => Str::random(10),
+            'user_id' => rand(1, 2)
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => Str::random(10),
+            'description' => Str::random(10),
+            'user_id' => rand(1, 2)
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => Str::random(10),
+            'description' => Str::random(10),
+            'user_id' => rand(1, 2)
+        ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +16,22 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'surname' => Str::random(10),
+            'nickname' => Str::random(10),
+            'age' => rand(0, 100),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'surname' => Str::random(10),
+            'nickname' => Str::random(10),
+            'age' => rand(0, 100),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
